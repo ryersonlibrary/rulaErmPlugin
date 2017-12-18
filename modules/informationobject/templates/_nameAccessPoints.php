56cc4bd752dd055ba52da8cea0dd68da45997895
Patrick Fung <patrick@makestuffdostuff.com>
2017-12-18 16:34:08 -0500

    remove condition for displaying in sidebar

--- modules/informationobject/templates/_nameAccessPoints.php
+++ modules/informationobject/templates/_nameAccessPoints.php
@@ -1,11 +1,9 @@
 <?php foreach ($resource->getActorEvents() as $item): ?>
   <?php if (isset($item->actor) && !isset($actorsShown[$item->actor->id])): ?>
   <div class="field">
-    <?php if (isset($sidebar)): ?>
-      <h3><?php echo $item->type->getRole() ?></h3>
+    <h3><?php echo $item->type->getRole() ?></h3>
 
-      <div><?php echo link_to(render_title($item->actor), array($item->actor)) ?></div>
-    <?php endif; ?>
+    <div><?php echo link_to(render_title($item->actor), array($item->actor)) ?></div>
   </div>
   <?php endif; ?>
 <?php endforeach; ?>
@@ -13,11 +11,9 @@
 <?php foreach ($resource->relationsRelatedBysubjectId as $item): ?>
   <?php if (isset($item->type) && QubitTerm::NAME_ACCESS_POINT_ID == $item->type->id): ?>
   <div class="field">
-    <?php if (isset($sidebar)): ?>
-      <h3><?php if (!isset($mods)): ?>(<?php echo __('Resource') ?>)<?php endif; ?></h3>
+    <h3><?php if (!isset($mods)): ?>(<?php echo __('Resource') ?>)<?php endif; ?></h3>
 
-      <div><?php echo link_to(render_title($item->object), array($item->object, 'module' => 'actor')) ?></div>
-    <?php endif; ?>
+    <div><?php echo link_to(render_title($item->object), array($item->object, 'module' => 'actor')) ?></div>
   </div>
   <?php endif; ?>
 <?php endforeach; ?>
